0ea3a77c9996f1006b0968989a24006f28a90d67
caffrey <caffreygo@163.com>
2019-03-21 16:00:44 +0800

    商品查询页面和接口对接

--- pages/select/select.js
+++ pages/select/select.js
@@ -1,16 +1,41 @@
 Page({
   data: {
-    listData: [
-      { "id": "001", "name": "名称", "price": "98.88" },
-      { "id": "002", "name": "名称", "price": "98.88" },
-      { "id": "003", "name": "名称", "price": "98.88" },
-      { "id": "004", "name": "名称", "price": "98.88" },
-      { "id": "005", "name": "名称", "price": "98.88" },
-      { "id": "006", "name": "名称", "price": "98.88" },
-      { "id": "007", "name": "名称", "price": "98.88" }
-    ]
+    goodsId: '',
+    goodsName: ''
   },
-
+  idInput: function({detail}) {
+    this.data.goodsId = detail.value
+  },
+  nameInput: function ({ detail }) {
+    this.data.goodsName = detail.value
+  },
+  submit: function(e) {
+    var that = this
+    if(this.data.goodsId=='' && this.data.goodsName==''){
+      wx.showToast({
+        title: '请先输入商品编号或名称！',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+    wx.request({
+      url: 'https://www.gocaffrey.xyz/PriceTagManager/servlet/search',  //服务器地址
+      data: {
+        goodsId: this.data.goodsId,
+        goodsName: this.data.goodsName
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' //默认值
+      },
+      success: function (res) {
+        let list = JSON.stringify(res.data)
+        wx.navigateTo({
+          url: '/pages/selectResult/selectResult?list=' + list,
+        })
+      }
+    })
+  },
 
   onLoad: function (options) {
 
